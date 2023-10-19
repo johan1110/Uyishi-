f7499f5d7f85a2ce9c5fd9b12cb7366b534bce04
johan1110 <johanmani100@gmail.com>
2023-10-19 15:25:01 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let count = 16
+function text(){
+    count = count +2 
+    document.body.style.fontSize = count + "px"
+}
+
+
+let count2 = 16
+function text2(){
+    count2 = count2 -2 
+    document.body.style.fontSize = count2 + "px"
+}
+
+const darc = function(){
+    document.body.style = "background-color:white; color:black;"
+}
+
+const darc2 = function(){
+    document.body.style = "background-color:black; color:white;"
+}
+
+let count3 = 16
+function pixil(){
+    count3 = count3 +25 
+    document.body.style.fontSize = count3 + "px"
+}
+
+let count4 = 16
+function pixil2(){
+    count4 = count4 +12 
+    document.body.style.fontSize = count4 + "px"
+}
+
+let count5 = 16
+function pixil3(){
+    count5 = count5 =16 
+    document.body.style.fontSize = count5 + "px"
+}
+
+
